32c0284637cf41d8cc866099f3142f695e540c33
Najam <najamaqeel20@gmail.com>
2022-12-08 02:57:23 +0500

    AutoCon 3.0

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, { useState } from "react";
-import { Tooltip } from 'primereact/tooltip';
 import ReactTooltip from 'react-tooltip';
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
 import "primereact/resources/primereact.min.css";                  //core css
@@ -9,8 +8,7 @@ import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import CodeEditor from '@uiw/react-textarea-code-editor';
 import Main from './components/Main';
-
-
+import { Tooltip } from 'primereact/tooltip';
 // On button click open pop up
 // 1: Enter file name
 // 2: Ok code comes left side
@@ -21,32 +19,20 @@ function App() {
   const [modal, setModal] = useState(false);
   const [filename, setFileName] = useState('');
   const [editor, setEditor] = useState(false);
-
   const [showIn, setshowIn] = useState(false);           // Hide and seek ....................
   const [showBottom, setshowBottom] = useState(false);           // Hide and seek ....................
   const [showBool, setshowBool] = useState(false);           // Hide and seek ....................
- 
+  const newLocal = <a href='http://localhost:3001/' className='cv-btn'> Let's Start</a>;
   const [code, setCode] = useState(
     `// SPDX-License-Identifier: MIT \n
-    pragma solidity >= 0.4.0 < 0.7.0;\n
-    \n`
-
-
+    pragma solidity >= 0.4.0 < 0.7.0;\n`
   );
-
-  const [s, sets] = useState('');  
-  const [s1, sets1] = useState(false);  
-  const [s2, sets2] = useState(false);  
-  const [s3, sets3] = useState(false);  
-  const [s4, sets4] = useState(false);  
-  const [s5, sets5] = useState(false);
-    
-
-  
 
   const toggleModal = () => {
     setModal(!modal);
   };
+   
+  
 
   const saveFileName = () => {
     if (filename !== '') {
@@ -71,13 +57,11 @@ function App() {
     
     const prevState = code;
     const editedText = prevState.slice(0, -1)
- 
     setEditor(true)
-    setCode( editedText + ` \n contract Sum { \n
+    setCode( editedText + `\n
+              contract Contract_Name { \n
               }`)
-    
-        sets1(true); //,............
-        setshowIn(true);   
+                             
 
   }
 
@@ -88,9 +72,11 @@ function App() {
     const editedText = prevState.slice(0, -1)
    
     setEditor(true)
-    setCode( editedText + ` uint storedData; \n  `);
-    setshowIn(true); // Hide and seek.......................
-
+    setCode( editedText + 
+        `\n uint storedData; \n  `);
+         setshowIn(true); // Hide and seek.......................
+         setshowBottom(true);
+        
   }
   const addBool= () => {
 
@@ -106,7 +92,8 @@ function App() {
     const editedText = prevState.slice(0, -1)// remove last curly brace
     // this will add the new line of code with the previous lines
     setEditor(true)
-    setCode( editedText +  `bool False; \n `);//remember to add '}' which we removed before
+    setCode( editedText +  
+        `bool False; \n `);//remember to add '}' which we removed before
                          
   }
   
@@ -117,56 +104,99 @@ function App() {
     const editedText = prevState.slice(0, -1)
    
     setEditor(true)
-    setCode( editedText +  `bool True; \n `);
+    setCode( editedText +  `   bool True; \n `);
+    setshowBottom(true);
                
   }
+  
 
   
   const addStringVariable = () => {
-
     
     const prevState = code;
     const editedText = prevState.slice(0, -1)
    
     setEditor(true)
-    setCode( editedText +  
-        `string Data; \n `);
+    setCode( editedText +  `   string Data; \n `);
+    setshowBottom(true);
+        
 
   }
 
   const addSetFunction = () => {
 
-    
     const prevState = code;
     const editedText = prevState.slice(0, -1)
     setEditor(true)
-   
-    setCode(editedText + `  
-              function set(uint x) public {\n
-               x = 5;\n
-               y = 6;\n
-              }
-            \n`);
-            //setshowIn(true); // Hide and seek.......................
-            sets5(true);
-
-  }
-  const addfirstFunction = () => {
-
-    
-    const prevState = code;
-    const editedText = prevState.slice(0, -1)
-    setEditor(true)
-    
     setCode(editedText + `
-             function firstNoSet(uint _x) public \n
-             {\n
-             First_Number = _x; 
-            
+              uint storedData; \n
+              function set(uint x) public {\n
+              storedData = x;\n
+              }
             }\n`);
-           
-            sets4(true); // ..........
+            setshowIn(true); // Hide and seek.
+
   }
+  const addConstructorFunction = () => {
+
+    const prevState = code;
+    const editedText = prevState.slice(0, -1)
+    setEditor(true)
+    setCode(editedText + `
+             constructor() public { \n
+             storedData = x; 
+            }\n`);
+ 
+
+  }
+
+
+  const addTemplate1 = () => {
+
+    const prevState = code;
+    const editedText = prevState.slice(0, -1)
+    setEditor(true)
+    setCode(editedText + `
+              
+              contract Shipping { \n
+              // Our predefined values for shipping listed as enums \n
+              enum ShippingStatus { Pending, Shipped, Delivered }  \n
+              // Save enum ShippingStatus in variable status \n
+              ShippingStatus private status; \n
+              // Event to launch when package has arrived\n
+              event LogNewAlert(string description);\n
+              // This initializes our contract state (sets enum to Pending once the program starts) \n
+              constructor() public {\n
+              status = ShippingStatus.Pending;\n
+              }\n
+              //Function to change to Shipped\n
+              function Shipped() public {\n
+              status = ShippingStatus.Shipped;\n
+              emit LogNewAlert("Your package has been shipped");\n
+               }\n
+               // Function to change to Delivered\n
+               function Delivered() public {\n
+                status = ShippingStatus.Delivered; \n
+                emit LogNewAlert("Your package has arrived"); \n
+              }\n
+              // Function to get the status of the shipping\n
+              function getStatus(ShippingStatus _status) internal pure returns (string memory) {\n
+              // Check the current status and return the correct name\n
+              if (ShippingStatus.Pending == _status) return "Pending";\n
+              if (ShippingStatus.Shipped == _status) return "Shipped";\n
+              if (ShippingStatus.Delivered == _status) return "Delivered";\n
+              // Get status of your shipped item \n
+              function Status() public view returns (string memory) {\n  
+                ShippingStatus _status = status; \n
+                return getStatus(_status);\n
+             
+            }\n
+
+          }\n`);
+ 
+
+  }
+
 
 
   const addGetFunction = () => {
@@ -174,78 +204,33 @@ function App() {
     const prevState = code;
     const editedText = prevState.slice(0, -1)
     setEditor(true)
-    
-    setCode( editedText + 
-             `
-              function get() view public  returns (uint)\n
-              {\n
-                uint output = First_Number + Second_Number;\n
-                return output;\n
+    setCode( editedText + ` function get() public view returns
+              (uint){\n
+              return storedData;\n
               }\n
             }\n`)
             setshowBottom(true);
-           
-    sets5(true);  //.............
-
 
   }
-  const addsecondfuction = () => {
- 
+
+  const addconditionFunction = () => {
+
+    const prevState = code;
+    const editedText = prevState.slice(0, -1)
+    setEditor(true)
+    setCode( editedText + 
+             ` if() \n
+                   {    }\n
+              else
+                  {    }\n
+            }\n`)
+            setshowBottom(true);
+
+  }
+
+
   
-    const prevState = code;
-    const editedText = prevState.slice(0, -1)
   
-    setEditor(true)
-    setCode( editedText + 
-             `
-               function secondNoSet(uint _y) public 
-              {
-                Second_Number = _y;
-              }
-             
-             
-             \n`)
-           
-             sets5(true); // ..........
-             //editor(true);
-             setshowIn(true);
-  }
-  const add1V = () => {
-
-    const prevState = code;
-    const editedText = prevState.slice(0, -1)
-   
-    setEditor(true)
-    setCode( editedText + 
-             ` 
-             uint First_Number ; \n
-             
-            `)
-          
-
-            sets2(true);  //............
-
-  }
-  const add2V = () => {
-
-    const prevState = code;
-    const editedText = prevState.slice(0, -1)
-   
-    setEditor(true)
-    setCode( editedText + 
-             ` 
-             uint Second_Number ; \n
-             
-            \n`)
-          
-
-            sets3(true);  //............
-
-  }
-
-
-
-
   return (
     <>
    
@@ -290,55 +275,61 @@ function App() {
 
       <div className='multi-button'>
       
-      <Button className="button"  label="Create Project"  icon="pi pi-plus" style={{marginRight: '1000px'}} onClick={toggleModal}/>
+  <Button className="button"  label="Create Project"  icon="pi pi-plus" style={{marginRight: '1000px'}} onClick={toggleModal}/>
   <a href="https://remix.ethereum.org/#optimize=false&runs=200&evmVersion=null&version=soljson-v0.8.7+commit.e28d00a7.jsgle.com" target="_blank" rel="noreferrer">
   <Button className="button"  tooltip="Remix IDE, is a no-setup tool with a GUI for developing smart contracts." tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} label="Remix"  icon="pi pi-plus" />
   </a>
 
+ 
 </div>
 
-      
-        <div className="split left"  >
-           
+
+        <div className="split left" >
+        
+          
+          
           <div className="centered">
 
           </div>
           <div class="container" >
           <div className='multi-button2'>
-
          
          
-          <br></br>
-          {filename ? <Button  tooltip="Startes with Contract Header as Class start" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Header" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "365px"}}  onClick={addTemplate} />
+        
+             {filename ? <Button  tooltip="Contract starts with Header" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}  className='p-button-sm'   label="Header"  iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "50px" }}  onClick={addTemplate} />
+            : ''}<ReactTooltip />
+            
+             {filename ? <Button  tooltip="shipping Contract - It tracks your skipment and show your updated status. " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Template 2" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "400px"}}  onClick={addTemplate1} />
             : ''}<ReactTooltip /><br></br>
-
           
-            <br></br>
-            {s1 ? <Button  tooltip="Add  First Variable " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="1st variable" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "260px"}}  onClick={add1V} />
-            : ''}<ReactTooltip /> 
-            {s2 ? <Button  tooltip="Add  Second Variable " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="2nd Variable" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "60px"}}  onClick={add2V} />
+            {filename ? <Button  tooltip=" Adds a Basic Function" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Function"  icon="pi pi-plus" iconPos="right"   style={{backgroundColor: '#2c3c60' , marginLeft: "50px"}}  onClick={addSetFunction} />
             : ''}<ReactTooltip /><br></br>
 
-          
-            <br></br>
-            {s3 ? <Button  tooltip="Gives your First variable Setter Function" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="First Setter Fucntion" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "320px"}}  onClick={addfirstFunction} />
+           
+            {showIn ? <Button  tooltip=" Adds a String Data Type Variable " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}    className='p-button-sm'  label="String Variables"  iconPos="right"  style={{backgroundColor: '#2c3c60' , marginLeft: "195px"}}  onClick={{addStringVariable} } />
+            : ''}<ReactTooltip />
+           
+            {showIn ? <Button  tooltip="Adds a Integer Data Type Variable " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}   className='p-button-sm'  label="int Variables"  iconPos="right"  style={{backgroundColor: '#2c3c60'}}   onClick={addVariable} />
+            : ''}<ReactTooltip />
+             {showIn ? <Button   tooltip="Adds a Boolean Data Type Variable " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}   className='p-button-sm'  label="Boolions"   iconPos="right"  style={{backgroundColor: '#2c3c60'}}   onClick={addBool} />
             : ''}<ReactTooltip /><br></br>
 
-       
+            {showBool ? <Button tooltip="Adds True Boolean " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}   className='p-button-sm'   label="Set Bool True"  iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "250px"}}  onClick={addBoolTrueVariable} />
+            : ''}<ReactTooltip />
+            {showBool ? <Button  tooltip="Adds False Boolean " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}  className='p-button-sm'   label="Set Bool False"  iconPos="right" style={{backgroundColor: '#2c3c60'}}  onClick={addBoolfalseVariable} />
+            : ''}<ReactTooltip /><br></br>
             <br></br>
-            {s4 ? <Button  tooltip="Gives your Second variable Setter Function" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Second Setter Function" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "310px"}}  onClick={addsecondfuction} />
+            {showBottom ? <Button  tooltip="Add Constructor" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}   className='p-button-sm'   label="Constructor"  icon="fas fa-caret-down"  iconPos="right"  style={{backgroundColor: '#2c3c60' , marginLeft: "310px"}}  onClick={addConstructorFunction} />
+            : ''}<ReactTooltip /><br></br>
+             {showBottom ? <Button tooltip="It Sets the Function " tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}  className='p-button-sm'   label="Set Function" icon="pi pi-plus" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "305px"}}    onClick={addSetFunction} />
+            : ''}<ReactTooltip /><br></br>
+            
+              {showBottom ? <Button  tooltip="It Gives If and Else Condition" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }}   className='p-button-sm'   label="Add Condtions"  iconPos="right"  style={{backgroundColor: '#2c3c60' , marginLeft: "310px"}}   onClick={addconditionFunction} />
+            : ''}<ReactTooltip /><br></br>
+               {showBottom ? <Button  tooltip="Give back Functions" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Get Function"  icon="pi pi-plus" iconPos="right" style={{backgroundColor: '#2c3c60', marginLeft: "305px"}}   onClick={addGetFunction} />
             : ''}<ReactTooltip /><br></br>
 
-            <br></br>
-
-            {s5 ? <Button  tooltip="Give Back Function" tooltipOptions={{ className: 'blue-tooltip', position: 'top' }} className='p-button-sm'   label="Getter Function" iconPos="right" style={{backgroundColor: '#2c3c60' , marginLeft: "335px"}}  onClick={addGetFunction} />
-            : ''}<ReactTooltip /><br></br>
-
-          
-            <br></br>
-            <br></br>
-
-          
+        
           </div>
          </div>
          </div>
@@ -357,7 +348,7 @@ function App() {
                 style={{
                   fontSize: 12,
                   
-                  backgroundColor: "#151B54",
+                  backgroundColor: "#2C3539",
                   fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
                 }}
                 
